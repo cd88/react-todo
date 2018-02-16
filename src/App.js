afe0e2d6dcd58a026160fd4bbf31069ed6117d3b
celloworld <camerondonahue@gmail.com>
2018-02-16 00:04:27 -0700

    simple react build
    using create-react-app, stripped down main app component already. Next few steps will roughly follow a tut from egghead.io

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  todo = {a: (state =  {}, action) => {
+
+  }};
+  render() {
+    return (
+      <div className="App">
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
